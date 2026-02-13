c69540772635fc990cd27cddb5366c17800b94eb
Jimmy Claw <jimmy-claw@users.noreply.github.com>
2026-02-13 11:47:48 +0100

    Add debug logging to guest

--- methods/guest/src/bin/treasury.rs
+++ methods/guest/src/bin/treasury.rs
@@ -5,16 +5,27 @@ use nssa_core::program::{AccountPostState, ProgramInput, read_nssa_inputs, write
 type Instruction = Vec<u8>;
 
 fn main() {
+    // Log that we're starting
+    println!("TREASURY: Guest starting");
+    
     let (
         ProgramInput { pre_states, instruction },
         instruction_words,
     ) = read_nssa_inputs::<Instruction>();
 
+    println!("TREASURY: Read {} pre_states", pre_states.len());
+    println!("TREASURY: Instruction length: {} bytes", instruction.len());
+    
     // For now, just pass through all accounts unchanged
     let post_states: Vec<AccountPostState> = pre_states
         .iter()
         .map(|pre| AccountPostState::new(pre.account.clone()))
         .collect();
+    
+    println!("TREASURY: Created {} post_states", post_states.len());
+    println!("TREASURY: Calling write_nssa_outputs");
 
     write_nssa_outputs(instruction_words, pre_states, post_states);
+    
+    println!("TREASURY: Guest complete!");
 }
