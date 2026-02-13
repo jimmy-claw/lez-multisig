40cd224986c7b63b327164da6108ac06e15303f7
Jimmy Claw <jimmy-claw@users.noreply.github.com>
2026-02-13 10:28:37 +0100

    Fix guest binary: use pre_states not accounts, remove program_id

--- methods/guest/src/bin/treasury.rs
+++ methods/guest/src/bin/treasury.rs
@@ -2,19 +2,17 @@
 
 #![no_main]
 
-use nssa_core::program::{read_nssa_inputs, write_nssa_outputs_with_chained_call, ProgramOutput};
+use nssa_core::program::{read_nssa_inputs, write_nssa_outputs_with_chained_call};
 use treasury_program;
 
 fn main() {
     // Read inputs from the zkVM environment
     let (program_input, instruction_words) = read_nssa_inputs::<treasury_program::Instruction>();
     
-    // Clone for process since it needs mutable references
-    let mut accounts = program_input.accounts.clone();
+    // Process the instruction (pre_states is now called pre_states)
+    let mut accounts = program_input.pre_states.clone();
     
-    // Process the instruction
     let output = treasury_program::process(
-        &program_input.program_id,
         &mut accounts,
         &program_input.instruction,
     );
@@ -22,7 +20,7 @@ fn main() {
     // Write outputs back to the zkVM
     write_nssa_outputs_with_chained_call(
         instruction_words,
-        program_input.accounts,
+        program_input.pre_states,
         output.post_states,
         output.chained_calls,
     );
